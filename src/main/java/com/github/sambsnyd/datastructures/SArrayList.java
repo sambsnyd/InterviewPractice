b72a9f0326cc11f81a93d873bc399d80d2b0584b
Sam <sambsnyder@gmail.com>
2018-11-14 23:09:30 -0800

    Started data structure

---
+++ src/main/java/com/github/sambsnyd/datastructures/SArrayList.java
@@ -0,0 +1,10 @@
+package com.github.sambsnyd.datastructures;
+
+/**
+ * Typical ArrayList
+ * Grows according to the rationale here
+ * https://sarahnothling.wordpress.com/2015/06/04/optimising-the-growth-factor-of-a-dynamic-array-list/
+ */
+public class SArrayList {
+
+}
